e3fda02705b676bd0283c26f18834d39dfbeb319
Jada <jada.fagan@gmail.com>
2020-06-23 15:40:21 -0400

    Add project files.

---
+++ ExceptionExamples/Exceptions/UserException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionExamples.Exceptions
+{
+    public class UserException : Exception 
+    {
+        public double NumberInError { get; set; }
+
+        public UserException() : base() { }
+
+        public UserException(string message) : base(message) { }
+
+        public UserException(string message, Exception innerException) : base(message, innerException) { }
+         //creating our userException from inherited exception?
+
+
+    }
+}
---
+++ ExceptionExamples/Program.cs
@@ -0,0 +1,62 @@
+﻿using ExceptionExamples.Exceptions;
+using System;
+
+namespace ExceptionExamples
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            try
+            {
+
+                Run();
+            }
+            catch (UserException ex)
+            {
+                Console.WriteLine($"{ex.Message}; number is {ex.NumberInError}.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+
+            }
+
+    static void Run()
+        {
+            Test();
+            var ex = new UserException("This exception was forced.");
+                ex.NumberInError = 36;
+                throw ex;
+        }
+
+        static void Test() {
+            Divide();
+        }
+
+        static void Divide()
+            {
+                try
+                {
+                    var num = 1;
+                    var denom = 0;
+                    var ans = num / denom;
+                }
+
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Exception occured: {ex.Message}.");
+                    //throw;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"General exception occured: {ex.Message}.");
+                    //can have as many catch blocks, need to gave them in order from specific to general exception
+                }
+
+            }
+        }
+
+    }
+}
